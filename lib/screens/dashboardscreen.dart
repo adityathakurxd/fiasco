6a449f4840434ef2926ee54eb2283c45e4ee0596
Aditya Thakur <adityathakurxd@users.noreply.github.com>
2021-08-10 20:42:29 +0530

    Feat: DONE Dashboard, Modified Home

--- lib/screens/dashboardscreen.dart
+++ lib/screens/dashboardscreen.dart
@@ -1,5 +1,6 @@
 import 'package:fiasco/constants.dart';
 import 'package:fiasco/screens/widgets/dashboardwidgets.dart';
+import 'package:fiasco/screens/widgets/expensewidgets.dart';
 import 'package:flutter/material.dart';
 
 class DashboardScreen extends StatelessWidget {
@@ -12,17 +13,71 @@ class DashboardScreen extends StatelessWidget {
         backgroundColor: kPrimaryColor,
         body: Column(
           children: [
-            SizedBox(
+            const SizedBox(
               height: 20,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                DashboardWidget(title: 'Income this month:', money: 5320, percent: 12, image: 'line-chart', profit: true,),
-                DashboardWidget(title: 'Costs this month:', money: 4122, percent: -46, image: 'bar-graph', profit: false,)
+                DashboardWidget(
+                  title: 'Income this month:',
+                  money: 5320,
+                  percent: 12,
+                  image: 'line-chart',
+                  profit: true,
+                ),
+                DashboardWidget(
+                  title: 'Costs this month:',
+                  money: 4122,
+                  percent: -46,
+                  image: 'bar-graph',
+                  profit: false,
+                )
               ],
             ),
-
+            const SizedBox(
+              height: 30,
+            ),
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 4.0,
+                      ),
+                    ]),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ExpenseWidget(
+                      imagePath: imageData[0],
+                      title: 'Mcdonalds',
+                      subtitle: 'Cafe and restaurant',
+                      expense: 20,
+                    ),
+                    ExpenseWidget(
+                      imagePath: imageData[1],
+                      title: 'Iphone 12 Pro',
+                      subtitle: 'Phones and accessories',
+                      expense: 999,
+                    ),
+                    ExpenseWidget(
+                      imagePath: imageData[2],
+                      title: 'Subscription',
+                      subtitle: 'Cinema and entertainment',
+                      expense: 9,
+                    ),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
